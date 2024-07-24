2d7fb3597b7c1a77f40cc7028326a16e7146a902
fen-gao <fernando.fnn97@gmail.com>
2024-07-24 20:08:56 -0300

    fix: change the drodown scrit

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,62 +1,46 @@
-/**
- * This function shows the dropdown.
- * @returns {void} This function does not return anything.
- */
 const showDropdown = () => {
+  console.log("showDropdown calledd");
   const inputField = document.getElementById("inputField");
   const dropdown = document.getElementById("dropdown");
-
   if (!inputField || !dropdown) {
-    console.error("Erro: Elementos necessários não encontrados.");
+    console.error("Erro: The dropdown or inputField element is not found.");
     return;
   }
 
   const rect = inputField.getBoundingClientRect();
-
   dropdown.style.top = `${rect.bottom + window.scrollY}px`;
   dropdown.style.left = `${rect.left + window.scrollX}px`;
   dropdown.style.display = "block";
+  console.log("Showing dropdown");
   document.getElementById("filterInput").focus();
 };
 
-/**
- * This function hides the dropdown.
- * @returns {void} This function does not return anything.
- */
 const hideDropdown = () => {
+  console.log("hideDropdown called");
   const dropdown = document.getElementById("dropdown");
-
   if (dropdown) {
     dropdown.style.display = "none";
     document.getElementById("filterInput").value = "";
     filterDropdown();
+    console.log("Dropdown hid");
   } else {
-    console.error("Erro: Element dropdown not found.");
+    console.error("Erro: The dropdown element is not found.");
   }
 };
 
-/**
- * This function handles the selection of a dropdown item.
- * @param {*} type The type of the dropdown item to be selected.
- * @returns {void} This function does not return anything.
- */
-const handleDropdownSelection = (type) => {
+function handleDropdownSelection(type) {
+  console.log("handleDropdownSelection chamada com tipo:", type);
   selectedType = getSelectedType(type);
-
   hideDropdown();
   addContent(selectedType);
   resetInputField();
-};
+}
 
-/**
- * This function filters the dropdown items.
- * @returns {void} This function does not return anything.
- */
-const filterDropdown = () => {
+function filterDropdown() {
+  console.log("filterDropdown chamada");
   const filterInput = document.getElementById("filterInput");
-
   if (!filterInput) {
-    console.error("Erro: Element filterInput not found.");
+    console.error("Erro: Elemento filterInput não encontrado.");
     return;
   }
 
@@ -64,7 +48,6 @@ const filterDropdown = () => {
   const items = document.querySelectorAll(".dropdown-item");
 
   let count = 0;
-
   items.forEach((item) => {
     const text = item.querySelector(".title").textContent.toLowerCase();
     if (text.includes(filterValue)) {
@@ -76,4 +59,5 @@ const filterDropdown = () => {
   });
 
   document.querySelector(".filtering-type").textContent = count;
-};
+  console.log("Dropdown filtrado, itens visíveis:", count);
+}
